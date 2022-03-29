7367695b9af1d2017ecb7f6e31e06829d3eac6e4
abhishek khajuria <63535532+abhishekhajuria@users.noreply.github.com>
2022-03-29 17:22:34 +0530

    Add files via upload

---
+++ Game code/Dice_Game_by_A206k.py
@@ -0,0 +1,42 @@
+import tkinter
+
+import PIL
+from PIL import Image, ImageTk
+import random
+
+root = tkinter.Tk()
+root.geometry('400x400')
+root.title('A206K Roll the Dice')
+
+l0 = tkinter.Label(root, text="")
+l0.pack()
+
+l1 = tkinter.Label(root, text="Hello from A206K!", fg = "light green",
+        bg = "dark green",
+        font = "Helvetica 16 bold italic")
+l1.pack()
+
+dice = ['die1.png', 'die2.png', 'die3.png', 'die4.png', 'die5.png', 'die6.png']
+
+###########.pack() – Used to pack the widget onto the root window######
+
+image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+
+label1 = tkinter.Label(root, image=image1)
+label1.image = image1
+
+# packing a widget in the parent widget 
+label1.pack( expand=True)
+
+# function activated by button
+def rolling_dice():
+    image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    # update image
+    label1.configure(image=image1)
+    # keep a reference
+    label1.image = image1
+
+button = tkinter.Button(root, text='Roll the Dice', fg='blue', command=rolling_dice)
+
+button.pack( expand=True)
+root.mainloop()
